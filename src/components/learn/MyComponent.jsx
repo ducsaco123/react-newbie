e1000157723693e77ed8cc7089a8ef1a61d3c7a9
ducsaco123 <ducnguyen230705@gmail.com>
2026-01-15 17:44:33 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,9 +1,16 @@
 //Component = html + css + js
+//JSX => Javascript XML: 1 parent
+//Fragment
+import './style.css';
+
 const MyComponent = () => {
     return (
         <>
             <div>
                 <p>Hello Sacogaming</p>
+            </div>
+            <div className="child">
+                <p>Child</p>
             </div>
         </>
     )
