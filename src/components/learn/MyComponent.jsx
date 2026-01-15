6ef91eaf51d20aa0f2ea6c77f18d9abf48b29b79
ducsaco123 <ducnguyen230705@gmail.com>
2026-01-15 17:22:53 +0700

    video 29

---
+++ src/components/learn/MyComponent.jsx
@@ -0,0 +1,12 @@
+//Component = html + css + js
+const MyComponent = () => {
+    return (
+        <>
+            <div>
+                <p>Hello Sacogaming</p>
+            </div>
+        </>
+    )
+}
+
+export default MyComponent;
---
+++ src/components/learn/SecondComponent.jsx
@@ -0,0 +1,21 @@
+const SecondComponent = () => {
+    return (
+        <>
+            <div>
+                <p>Second Component</p>
+            </div>
+        </>
+    );
+}
+
+const ThirdComponent = () => {
+    return (
+        <>
+            <div>
+                <p>Third Component</p>
+            </div>
+        </>
+    );
+}
+
+export { SecondComponent, ThirdComponent };
