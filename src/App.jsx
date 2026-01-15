1a4484252e6b00801f0b3c238380c511c6861616
ducsaco123 <ducnguyen230705@gmail.com>
2026-01-15 16:19:24 +0700

    video 27

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const myFunction = () => {
+    console.log('This is a function');
+  }
+  myFunction();
 
   return (
     <>
