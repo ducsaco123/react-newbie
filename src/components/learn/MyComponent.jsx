98c733ff1bb487e73f2cd759ae81fed3be4d238f
ducsaco123 <ducnguyen230705@gmail.com>
2026-01-16 22:29:18 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -4,10 +4,23 @@
 import './style.css';
 
 const MyComponent = () => {
+    // const test = 'Sacogaming'; //String
+    // const test = 123; //Number
+    // const test = true; //Boolean
+    // const test = undefined; //Undefined
+    // const test = null; //Null
+    // const test = [1, 2, 3]; //Array
+    const test = {
+        name: 'Sacogaming',
+        age: 20
+    }
     return (
         <>
             <div>
-                <p>Hello Sacogaming</p>
+                <p>Hello {JSON.stringify(test)}</p>
+            </div>
+            <div>
+                {console.log('Saco')}
             </div>
             <div className="child">
                 <p>Child</p>
