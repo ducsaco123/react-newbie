e116b8a277cb06e26f5f2385792cffcf96d57795
ducsaco123 <ducnguyen230705@gmail.com>
2026-01-17 03:33:30 +0700

    video 36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,11 +1,19 @@
 const TodoNew = (props) => {
     const { addNewTodo } = props;
     // addNewTodo('Saco');
+
+    const handleOnClick = () => {
+        alert('Clicked');
+    }
+
+    const handleOnChange = (name) => {
+        console.log(name);
+    }
     return (
         <>
             <div className="todo-input-container">
-                <input type="text" className='todo-input' placeholder='Enter your task...' />
-                <button className='todo-button'>Add</button>
+                <input type="text" className='todo-input' placeholder='Enter your task...' onChange={(e) => handleOnChange(e.target.value)} />
+                <button className='todo-button' onClick={handleOnClick}>Add</button>
             </div>
         </>
     )
